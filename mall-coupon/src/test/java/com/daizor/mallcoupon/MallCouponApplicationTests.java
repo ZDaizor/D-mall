bdf4f97f39999ae0adbd6dd4f94d06d5b32813a2
ZDaizor <xingzuozhou@gmail.com>
2020-09-15 19:32:25 +0800

    初始化结构

---
+++ mall-coupon/src/test/java/com/daizor/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daizor.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
